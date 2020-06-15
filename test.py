736fc52281563f20682d45cc880bc14874915ef6
Nishant Aman <Nishantaman36@gmail.com>
2020-06-16 02:02:40 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+from flask import Flask,request
+from twilio.twiml.messaging_response import MessagingResponse
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    response ='test karatani'
+    if request.method == 'POST':
+        incoming_msg = request.values.get('Body', '')
+
+        # create Twilio XML response
+        resp = MessagingResponse()
+        msg = resp.message()
+
+        if incoming_msg == 'hello':
+            response = "*Hi! I am the Quarantine Bot*"
+            msg.body(response)
+    
+        return str(response)
+    return str(response) 
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
