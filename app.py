bba74d9f33f4c72be56b669445eb54973a15d8d4
Nishant Aman <Nishantaman36@gmail.com>
2020-06-16 11:26:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask,request
 from twilio.twiml.messaging_response import MessagingResponse
+import re
+import random
+
 app = Flask(__name__)
 
 
@@ -9,18 +12,33 @@ def hello():
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    response ='test karatani'
-    print(request.method)
     if request.method == 'POST':
         incoming_msg = request.values.get('Body', '')
+        # incoming_msg = request.json.get('Body', '')
+        response = map_incoming_msg(incoming_msg)
 
         # create Twilio XML response
         resp = MessagingResponse()
         msg = resp.message()
-        response = "*Hi! I am the Quarantine Bot*"
         msg.body(response)
     
     return str(resp)
 
+
+@app.route('/email', methods=['POST'])
+def email_extractor():
+    incoming_msg = request.json.get('Body')
+    email_matches = re.findall(r'[\w\.-]+@[\w\.-]+',incoming_msg) 
+    return {"email_matches":email_matches}   
+
+def map_incoming_msg(text):
+    greeting = {'input':['hi','hello','hey','Wassup'],'output':['Aur bc kya chal rha','Chutiye kuch karle']}
+    default_text = 'Kya bol raha bhosdike'
+    if text in greeting['input']:
+        output_list = greeting['output'] 
+        length = len(output_list)   
+        return output_list[random.randint(0,length-1)]
+    return default_text
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000,debug=True)
